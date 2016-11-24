4ca00f9a860a8783f41da52c7c3603345e63e2e5
xun <mx@rmbz.net>
2016-11-24 23:46:13 +0800

    同步 cookies

--- src/main/java/net/rmbz/demo/App.java
+++ src/main/java/net/rmbz/demo/App.java
@@ -10,46 +10,80 @@ import java.util.List;
  */
 public class App {
 
-    public static void main(String[] args) {
+    private final static String URL_GET = "http://www.baidu.com";
+
+    private final static String URL_POST = "http://requestb.in/17sbe751";
+
+    private final static String COOKIE_KEY = "cookie";
+
+    private final static String SET_COOKIE_KEY = "set-cookie";
+
+    private final static MediaType MEDIA_TYPE_TEXT = MediaType.parse("text/html; charset=utf-8");
+
+    private static ResultVo result = new ResultVo();
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
-        Request request = new Request();
+        get();
+        post();
     }
 
-    private Request networkRequest(Request request) throws IOException {
-        Request.Builder result = request.newBuilder();
-
+    public static void get() throws IOException {
         OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(URL_GET).build();
+        okhttp3.Response response = client.newCall(request).execute();
+        String cookies = response.header(SET_COOKIE_KEY, null);
+        result.setBody(response.body().string());
+        result.setCookies(cookies);
+        if (response.isSuccessful()) {
+            System.out.println("##### " + result);
+        } else {
+            System.err.println("##### okHttp is request error");
+        }
+    }
 
-        List<Cookie> cookies = client.cookieJar().loadForRequest(request.url());
-        if (!cookies.isEmpty()) {
-            result.header("Cookie", cookieHeader(cookies));
+    public static void post() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(URL_POST)
+                .header(COOKIE_KEY, result.getCookies())
+                .post(RequestBody.create(MEDIA_TYPE_TEXT, result.getBody()))
+                .build();
+        okhttp3.Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            System.out.println("##### " + response.body().string());
+        } else {
+            System.err.println("##### okHttp is request error");
+        }
+    }
+
+    protected static class ResultVo {
+        private String body;
+        private String cookies;
+
+        public String getBody() {
+            return body;
         }
 
-        //例行省略....
-
-        return result.build();
-    }
-
-    private String cookieHeader(List<Cookie> cookies) {
-        StringBuilder cookieHeader = new StringBuilder();
-        for (int i = 0, size = cookies.size(); i < size; i++) {
-            if (i > 0) {
-                cookieHeader.append("; ");
-            }
-            Cookie cookie = cookies.get(i);
-            cookieHeader.append(cookie.name()).append('=').append(cookie.value());
+        public void setBody(String body) {
+            this.body = body;
         }
-        return cookieHeader.toString();
-    }
 
-    public void receiveHeaders(Request request, Headers headers) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-        if (client.cookieJar() == CookieJar.NO_COOKIES) return;
+        public String getCookies() {
+            return cookies;
+        }
 
-        List<Cookie> cookies = Cookie.parseAll(request.url(), headers);
-        if (cookies.isEmpty()) return;
+        public void setCookies(String cookies) {
+            this.cookies = cookies;
+        }
 
-        client.cookieJar().saveFromResponse(request.url(), cookies);
+        @Override
+        public String toString() {
+            return "ResultVo{" +
+                    "body='" + body + '\'' +
+                    ", cookies='" + cookies + '\'' +
+                    '}';
+        }
     }
 
 }
